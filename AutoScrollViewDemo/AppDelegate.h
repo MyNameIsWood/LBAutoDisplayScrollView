96c5966cb3af917c7222e74334addc242237d338
aios <aios@aios.local>
2015-08-31 17:23:32 +0800

    Initial Commit

---
+++ AutoScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoScrollViewDemo
+//
+//  Created by aios on 15/8/31.
+//  Copyright (c) 2015年 BlueMobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoScrollViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoScrollViewDemo
+//
+//  Created by aios on 15/8/31.
+//  Copyright (c) 2015年 BlueMobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
